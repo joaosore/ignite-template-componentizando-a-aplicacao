1c7977696e65921e355d19a90d56c482158687ec
João Marcos <joao.marcos@medway.com.br>
2022-04-19 14:16:43 -0300

    Start

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+import '../styles/sidebar.scss';
+
+import { Button } from '../components/Button';
+
+import { api } from '../services/api';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface Props {
+  handleClickButton: any;
+  selectedGenreId: number;
+}
+
+export function SideBar({ selectedGenreId, handleClickButton }: Props) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
